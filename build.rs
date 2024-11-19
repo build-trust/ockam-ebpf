e0f6134ec916fed29db4655c3baff632c0d7bd0e
Oleksandr Deundiak <olek@ockam.io>
2024-11-19 18:02:53 +0100

    f

--- build.rs
+++ build.rs
@@ -11,7 +11,7 @@ fn build_ebpf() {
     let output_file = out_dir.join("ockam_ebpf");
 
     let target_dir = out_dir.join("ebpf");
-    _ = std::fs::remove_dir(&target_dir);
+    std::fs::remove_dir(&target_dir).unwrap();
     std::fs::create_dir(&target_dir).unwrap();
 
     let output = Command::new("cargo")
